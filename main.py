b86da795715f551c035be1e13a6289585674acef
marcdeverteuil <marc.o.deverteuil@gmail.com>
2019-11-01 11:34:17 -0400

    Added transfer tests

--- main.py
+++ main.py
@@ -45,11 +45,13 @@ def controller(user_input):
             daily_withdrawals = new_daily_withdrawals
             print("Successful withdrawal")
     elif user_input == ("transfer"):
-        transaction, new_daily_transfers = transfer(mode, daily_transfers)
-        daily_transfers = new_daily_transfers
-        if transaction != False:
-            transaction_list.append(transaction)
-            print("Successful transfer")
+        account_num, amount, account_num2 = getFeatureInput('transfer')
+        if (account_num and amount and account_num2) != False:
+            transaction, new_daily_transfers = transfer(mode, account_num, account_num2, amount, daily_transfers)
+            daily_transfers = new_daily_transfers
+            if transaction != False:
+                transaction_list.append(transaction)
+                print("Successful transfer")
     else: 
         print("That is not a recognized command")
 
@@ -69,6 +71,17 @@ def getFeatureInput(feature):
     except:
         print("Invalid %s amount" % feature)
         return False, False
+    if(feature == 'transfer'):
+        try:
+            account_num2 = int(input("Please enter account number to transfer to: "))
+        except:
+            print("Invalid account number")
+            return False, False, False
+        if(not validateAccountNumber(7654321)): 
+            print("Invalid account number")
+            return False, False
+        else:
+            return account_num, amount, account_num2
     return account_num, amount
 
 def login():
