6fc9c371c44299187f0e823e61c53819f1239ee1
tedward <tedward.munn@gmail.com>
2019-11-01 20:42:18 -0400

    completed tests for withdraw

--- test_withdraw.py
+++ test_withdraw.py
@@ -32,20 +32,38 @@ def test_withdraw_success_machine():
     assert transaction == expected_transaction
     assert daily_withdrawals == new_daily_withdrawals
 
-@pytest.mark.skip(reason = "getting error with account input")
-def test_withdraw_invalid_account_leading_0():
-    #should we do this with agent or machine or both?
+def test_withdraw_invalid_amount_atm():
     daily_withdrawals = {1234567: 0}
-    account_num = 0123456
-    amount = 10000
+    account_num = 1234567
+    amount = 200000
     #agent mode
     mode = 0
 
     transaction, new_daily_withdrawals = withdraw(account_num, amount, mode, daily_withdrawals)
-    expected_transaction = Transaction("WDR", account_num, amount, "0000000", '***').create_transaction_line()
-    transaction = transaction.create_transaction_line()
-    daily_withdrawals[account_num] = amount
 
-    #daily_withdrawals are the same?
     assert transaction == False
-    assert daily_withdrawals == new_daily_withdrawals
+    assert new_daily_withdrawals == daily_withdrawals
+
+def test_withdraw_invalid_amount_agent():
+    daily_withdrawals = {1234567: 0}
+    account_num = 1234567
+    amount = 999999999
+    #agent mode
+    mode = 1
+
+    transaction, new_daily_withdrawals = withdraw(account_num, amount, mode, daily_withdrawals)
+
+    assert transaction == False
+    assert new_daily_withdrawals == daily_withdrawals
+
+def test_withdraw_invalid_total_amount_atm():
+    daily_withdrawals = {1234567: 490000}
+    account_num = 1234567
+    amount = 200000
+    #agent mode
+    mode = 0
+
+    transaction, new_daily_withdrawals = withdraw(account_num, amount, mode, daily_withdrawals)
+
+    assert transaction == False
+    assert new_daily_withdrawals == daily_withdrawals
